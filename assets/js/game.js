a6d8c3e6be633b412db69a58ebd223b7c2bf2373
Leandri Beavis <leandrikuyk@gmail.com>
2022-04-14 21:48:37 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,30 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
+// console.log(playerName, playerHealth, playerAttack);
 
-console.log(playerName, playerHealth, playerAttack);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// console.log(enemyNames);
+
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index");
+// }
+
 // this creates a function named "fight by using a function expression"
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting this round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -53,12 +67,17 @@ var fight = function() {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping
             playerMoney = playerMoney - 2;
+        } else {
+            // if no (false), ask question again by running fight() again
+            fight();
         }
         // if no (false), ask question again by running fight() again
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
-}
+};
 
 // execute function
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
