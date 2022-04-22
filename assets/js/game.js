a605e3b0ac1cf95e28a0e4725d4841fb1b6aa396
Leandri Beavis <leandrikuyk@gmail.com>
2022-04-22 19:57:33 -0400

    fixed empty player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,8 +69,21 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
+
 var playerInfo = {
-    name: window.prompt("What is your robot name?"),
+    name: getPlayerName(),
     health: 100, 
     attack: 10,
     money: 10,
@@ -126,6 +139,8 @@ var startGame = function() {
     for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+            // debugger;
 
             // pick new enemy to fight based on the index of the enemy.names array
             var pickedEnemyObj = enemyInfo[i];
